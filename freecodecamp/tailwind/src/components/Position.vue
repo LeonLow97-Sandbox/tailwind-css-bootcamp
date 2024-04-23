2050519f9af78f2fb9ee505b28866aedd010d923
LeonLow97 <lowjiewei@gmail.com>
2024-04-23 22:11:40 +0800

    Tailwind object positioning

--- freecodecamp/tailwind/src/components/Position.vue
+++ freecodecamp/tailwind/src/components/Position.vue
@@ -2,10 +2,15 @@
   <div class="h-screen bg-slate-900 text-white">
     <div class="container px-2">
       <div class="p-8 w-32 h-32 relative rounded-lg bg-red-500">
-        <div
-          class="w-24 h-24 absolute inset-2 rounded-lg bg-green-500"
-        ></div>
+        <div class="w-24 h-24 absolute inset-2 rounded-lg bg-green-500"></div>
       </div>
+    </div>
+
+    <div class="container pt-5">
+      <img
+        class="w-64 h-64 object-none object-center"
+        src="https://source.unsplash.com/random"
+      />
     </div>
   </div>
 </template>
